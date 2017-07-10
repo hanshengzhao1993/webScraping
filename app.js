8e299a7aa919e6898dabde162ff47b8dce10852d
Han <hanshengzhao1993@gmail.com>
2017-07-09 21:43:33 -0700

    Testing on multiple websites

--- app.js
+++ app.js
@@ -7,19 +7,30 @@ var cheerio = require('cheerio');
 var app = express();
 
 app.get('/scape', function (req, res) {
+  var listOfUrls = ["https://jobs.lever.co/nima", "https://jobs.lever.co/addepar", 'https://jobs.lever.co/hellosign', 'https://jobs.lever.co/fathomhealth', 'https://jobs.lever.co/fathom', 'https://jobs.lever.co/fat'];
+  var answers = [];
 
   // Where the web scraping will be done
-  var url = "https://jobs.lever.co/nima";
+  // var url = "https://jobs.lever.co/nima";
 
-  request(url, function (error, response, html) {
-    if(!error){
-      console.log(html);
-      console.log(html.includes('San Francisco'), html.includes('Software'))
-      var $ = cheerio.load(html);
-      // console.log('What is the dollar sign: ',$)
-    }
-  })
+  listOfUrls.forEach(function (url) {
+    request(url, function (error, response, html) {
+      if(!error){
+        // console.log(html.includes('San Francisco'), html.includes('Software'))
+        var $ = cheerio.load(html);
+        console.log('THIS IS THE RESPONSE: ', response.statusCode)
+        console.log(`THIS IS THE URL: ${url}`, $('body').text());
+        // console.log('What is the dollar sign: ',$)
+        // if( html.includes('San Francisco') ){
+        //   console.log('in here?')
+        //   answers.push(url);
+        // }
+      }
+    });
+  });
+  console.log(answers)
 
+  res.send(answers);
 
 })
 
